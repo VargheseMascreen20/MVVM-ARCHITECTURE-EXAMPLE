90bc2e7a1725377c505ff5a9d7257fd15715fe08
varghese <varghese.t.mascreen20@gmail.com>
2022-04-09 10:58:05 +0530

    First Commit

---
+++ app/src/main/java/com/var/mvvmarchitecture/MainActivity.java
@@ -0,0 +1,31 @@
+package com.var.mvvmarchitecture;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.databinding.BindingAdapter;
+import androidx.databinding.DataBindingUtil;
+
+import com.var.mvvmarchitecture.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindingAdapter({"toastMessage"})
+    public static void runMe(View view, String message) {
+        if (message != null)
+            Toast.makeText(view.getContext(), message, Toast.LENGTH_SHORT).show();
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+
+        ActivityMainBinding activityMainBinding = DataBindingUtil.setContentView(this, R.layout.activity_main);
+        activityMainBinding.setViewModel(new AppViewModel());
+        activityMainBinding.executePendingBindings();
+
+    }
+}
